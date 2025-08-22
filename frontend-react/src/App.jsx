68abf4b4956be2718f956a83ad2b6ed4c67e671f
Rekha718 <rekha2002718@gmail.com>
2025-08-22 21:17:28 +0530

    Home Page React

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from 'react'
-
+import "./assets/css/style.css"
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Main from './components/Main'
 function App() {
 
   return (
     <>
-      <h1>Stock Prediction Portal</h1>
+      <Header/>
+      <Main/>
+      <Footer/>
+      
     </>
   )
 }
---
+++ frontend-react/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+    <footer>
+        <div className="footer py-3 my-3">
+            <hr className='border-bottom'/>
+            <p className='text-light text-center'> &copy; 2025 - Built with ❤️ by Rekha.</p>
+        </div>
+    </footer>
+    </>
+  )
+}
+
+export default Footer
